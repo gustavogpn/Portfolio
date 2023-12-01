30f0e0698e27a0730b729c4b2db82b7c4db2b195
Gustavo Gomes <gustavogpn01@gmail.com>
2023-12-01 19:01:49 -0300

    Sobre mim atualizado

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const TopLeftImg = () => {
     return (
-    <div className='absolute left-0 top-0 mix-blend-color-dodge w-[200px] xl:w-[400px] opacity-50 z-10'>
+    <div className='absolute left-0 top-0 mix-blend-color-dodge w-[200px] xl:w-[400px] opacity-50'>
       <Image 
       className='animate-pulse duration-100'
       src='/top-left-img.png' 
--- pages/about/index.js
+++ pages/about/index.js
@@ -6,7 +6,6 @@ import {
   FaCss3,
   FaJs,
   FaWordpress,
-  FaFigma,
   FaPhp,
 } from "react-icons/fa";
 
@@ -28,7 +27,7 @@ const aboutData = [
     title: 'habilidades',
     info: [
       {
-        title: 'Web Development',
+        title: 'Desenvolvimento Web',
         icons: [
           <FaHtml5 />,
           <FaCss3 />,
@@ -40,7 +39,6 @@ const aboutData = [
       {
         title: 'UI/UX Design',
         icons: [
-        <FaFigma />, 
         <SiAdobexd />, 
         <SiAdobephotoshop />
       ],
@@ -48,59 +46,107 @@ const aboutData = [
     ],
   },
   {
-    title: 'awards',
+    title: 'Experiência',
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: 'Desenvolvedor Web - Freelancer',
+        stage: '2020 - 2023',
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: 'Empreendedor - Agência Digital',
+        stage: '',
       },
     ],
   },
   {
-    title: 'experience',
+    title: 'credenciais',
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: 'Graduado em Análise e Desenvolvimento de Sistemas',
+        stage: '2022 - 2024',
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: 'Dezenas de cursos e certificados',
+        stage: '',
       },
     ],
   },
 ];
 
+//framer motion
+import {motion} from 'framer-motion';
+import {fadeIn} from '../../variants';
+
 const About = () => {
   const[index, setIndex] = useState(0)
-  return <div>
+  console.log(index);
+  return (
+   <div className='h-full bg-primary/30 py-32 text-center xl:text-left'
+   >
     <Circles />
-  </div>;
+    {/* avatar img */}
+    <motion.div 
+    variants={fadeIn('right', 0.2)}
+    initial='hidden'
+    animate='show'
+    exit='hidden'
+    className='hidden xl:flex absolute bottom-0 -left-[370px]'
+    >
+      <Avatar />
+    </motion.div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-row 
+    gap-x-6'>
+      {/* text */}
+      <div className='flex-1 flex flex-col justify-center'>
+        <h2>Boas <span>Ideas </span>Criam Designs Fantásticos.</h2>
+        <p>Há 3 anos, eu comecei com o Desenvolvimento Web, desde então estudo constantemente 
+        as tecnologias mais inovadoras. Já participei de vários projetos, com ênfase em sites 
+        institucionais, landing pages e e-commerces. Graduado em Análise e Desenvolvimento
+        de Sistemas, faço a ponte entre sua ideia e um negócio de sucesso.</p>
+      </div>
+      {/* info */}
+        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => {
+             return (
+              <div 
+                key={itemIndex}
+                className={`${
+                  index === itemIndex 
+                  && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+              } cursor-pointer capitalize xl:text-lg relative after:w-8
+                after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+              );
+            })}
+          </div>
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4
+          items-center xl:items-start'>
+            {aboutData[index].info.map((item, itemIndex)=>  {
+              return (
+                <div key={itemIndex} className='flex-1 flex flex-col md:flex-row
+                max-w-max gap-x-2 items-center text-white/60'>
+                  {/* title */}
+                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                  <div className='hidden md:flex'></div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4'>
+                      {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className='text-2xl text-white'>{icon}</div>
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
